74a81107019b0960a601f70c52bd9a082e70b11f
craigrusselltiu <craigrusselltiu@users.noreply.github.com>
2021-03-10 03:03:24 +1100

    Updates

---
+++ precache-manifest.b2906b02b6bbffaa3c170d8aea7e708b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc50b921a29f60e8729dc96eb44d6c86",
+    "url": "/simple-cart/index.html"
+  },
+  {
+    "revision": "b46f3082938088058ca6",
+    "url": "/simple-cart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "afca47a5a9c61a59ff11",
+    "url": "/simple-cart/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b46f3082938088058ca6",
+    "url": "/simple-cart/static/js/2.2792a3c4.chunk.js"
+  },
+  {
+    "revision": "afca47a5a9c61a59ff11",
+    "url": "/simple-cart/static/js/main.67348233.chunk.js"
+  },
+  {
+    "revision": "cba60625f3f6c32a1185",
+    "url": "/simple-cart/static/js/runtime~main.8006e4b9.js"
+  }
+]);
